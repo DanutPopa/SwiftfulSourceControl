104a5110e734c75936c38eec8253282895c45716
DanutPopa <popa.ion.danut@gmail.com>
2025-01-20 18:20:33 +0200

    [Feature] Add SettingsView

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Danut Popa on 20.01.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings!")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
