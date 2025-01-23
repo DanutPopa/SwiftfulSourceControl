ce028f3560979674c0fd9f29a3aecf96b245c8f3
DanutPopa <popa.ion.danut@gmail.com>
2025-01-23 15:37:50 +0200

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Danut Popa on 23.01.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nick!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
